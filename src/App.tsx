3e0e1c73e64581d2465b1d474e115a6de2fc1f31
Bruno Silva <bsilva18239@icloud.com>
2026-02-25 11:38:16 -0300

    Exibe FormVagas no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import FormVagas from './components/FormVagas'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <FormVagas />
       <div className="container">
         <ListaVagas />
       </div>
